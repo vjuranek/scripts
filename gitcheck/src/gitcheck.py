00ce0223c6484c1a4e67a63de8c1c01a94cc5a99
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-02-26 14:20:41 +0100

    Use only logging

--- gitcheck/src/gitcheck.py
+++ gitcheck/src/gitcheck.py
@@ -2,18 +2,18 @@
 
 from git import *
 import configparser
-import os,sys
+import os
+import sys
 import logging
 
 # Setup logging
-LOG_LEVELS = ["CRITICAL","ERROR","WARNING","INFO","DEBUG"]
 logging.basicConfig(level=logging.INFO)
 
 # Load configuration
 # configuration has to be stored in $HOME/.gitcheck/gitcheck.conf
 userHome = os.getenv("HOME")
 if not userHome:
-    print("$HOME is not set - terminating ...")
+    logging.error("$HOME is not set - terminating ...")
     sys.exit(1)
 logging.debug("User home: " + userHome)
 CFG_PATH = userHome + "/.gitcheck/gitcheck.conf"
@@ -21,24 +21,25 @@ config = configparser.ConfigParser()
 config.read(CFG_PATH)
 
 logLevel = config.get("LOGGING","level")
-if not LOG_LEVELS.__contains__(logLevel):
-    print("Config error: bad logging level - terminating ...")
+if not logLevel in logging._nameToLevel:
+    logging.error("Config error: bad logging level - terminating ...")
     sys.exit(0)
+logging.getLogger().setLevel(logLevel)
 
 dirsCfg = config.get("DIRS TO CHECK","dirs")
 logging.debug("Dirs to be checked: " + dirsCfg)
 if not dirsCfg:
-    print("No directories to be checked are configured - exiting ...")
+    logging.info("No directories to be checked are configured - exiting ...")
     sys.exit(0)
 dirs = dirsCfg.split('\n') # list of directories, each on separate line
 
 # check each directory
 for dir in dirs:
     repo = Repo(dir)
-    print(repo.is_dirty())
+    logging.debug(repo.is_dirty())
     if len(repo.untracked_files) == 0:
-        print("No untracked files")
+        logging.info("No untracked files")
     else:
-        print("Untacked files:")
+        logging.info("Untacked files:")
         for file in repo.untracked_files:
-            print("\t" + file)
+            logging.info("\t" + file)
